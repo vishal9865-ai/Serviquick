4187e3ef681b1906a051ce4cb22f18ddb21f5923
vishal9865-ai <vishalchourasiya071@gmail.com>
2025-11-04 23:42:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const links = document.querySelectorAll('.nav-links a');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
